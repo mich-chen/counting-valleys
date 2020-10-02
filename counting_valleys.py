4bd76622b462568810407afbd884ff0cb9bcea71
Michelle Chen <mich.chen.94@gmail.com>
2020-10-02 16:21:24 -0700

    brute force solution

---
+++ counting_valleys.py
@@ -0,0 +1,30 @@
+def countingValleys(steps, path):
+
+    valleys = 0
+    valley_down = False
+    valley_up = False
+    sealevel = 0
+
+    for i in range(steps):
+        if path[i] == 'D':
+            if sealevel == 0:
+                valley_down = True
+            sealevel -= 1
+
+        if path[i] == 'U':
+            sealevel += 1
+            if sealevel == 0:
+                valley_up = True
+
+        if valley_down == True and valley_up == True:
+            valleys += 1
+            valley_down = valley_up = False
+
+    return valleys
+
+
+if __name__ == '__main__':
+
+    print(countingValleys(8, "UDDDUDUU"))
+    print(countingValleys(12, "DDUUDDUDUUUD"))
+    
