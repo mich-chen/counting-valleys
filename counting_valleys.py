afed85b12a545d53671cd931ddf32e3be2641823
Michelle Chen <mich.chen.94@gmail.com>
2020-10-02 16:35:22 -0700

    optimized

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,24 +1,29 @@
 def countingValleys(steps, path):
 
     valleys = 0
-    valley_down = False
-    valley_up = False
+    # valley_down = False
+    # valley_up = False
     sealevel = 0
 
     for i in range(steps):
         if path[i] == 'D':
-            if sealevel == 0:
-                valley_down = True
+            # if sealevel == 0:
+            #     valley_down = True
             sealevel -= 1
 
         if path[i] == 'U':
             sealevel += 1
-            if sealevel == 0:
-                valley_up = True
+            # if sealevel == 0:
+            #     valley_up = True
 
-        if valley_down == True and valley_up == True:
+        # if reached sealevel from an UP then exiting a valley
+        if sealevel == 0 and path[i] == 'U':
             valleys += 1
-            valley_down = valley_up = False
+
+        # alternative brute force solution:
+        # if valley_down == True and valley_up == True:
+        #     valleys += 1
+        #     valley_down = valley_up = False
 
     return valleys
 
